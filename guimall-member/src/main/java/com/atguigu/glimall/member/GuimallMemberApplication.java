c96431a51f031c5641bfe59fd502045f59861138
wx1655 <819269647@qq.com>
2021-02-17 17:42:59 +0800

    1. 逆向工程 快速完成微服务的CRUD 2. 模板脚手架 快速搭建后台微服务 3. 网关demo 4. openfeign 5. nacos的服务注册发现与配置中心等

--- guimall-member/src/main/java/com/atguigu/glimall/member/GuimallMemberApplication.java
+++ guimall-member/src/main/java/com/atguigu/glimall/member/GuimallMemberApplication.java
@@ -2,8 +2,12 @@ package com.atguigu.glimall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "com.atguigu.glimall.member.feign")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GuimallMemberApplication {
 
     public static void main(String[] args) {
--- guimall-product/src/test/java/com/atguigu/glimall/product/GuimallProductApplicationTests.java
+++ guimall-product/src/test/java/com/atguigu/glimall/product/GuimallProductApplicationTests.java
@@ -1,13 +1,38 @@
 package com.atguigu.glimall.product;
 
+import com.atguigu.glimall.product.entity.BrandEntity;
+import com.atguigu.glimall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.function.Consumer;
 
 @SpringBootTest
 class GuimallProductApplicationTests {
+    /**
+     * 注入service
+     * 测试里面提供的增删改查方法
+     */
+    @Autowired
+    BrandService brandService;
 
     @Test
     void contextLoads() {
+        BrandEntity entity = new BrandEntity();
+        entity.setBrandId(13L);
+        entity.setDescript("This is the first work");
+//        entity.setName("Huawei");
+//        brandService.save(entity);
+//        System.out.println("保存成功...");
+
+//        brandService.updateById(entity);
+//        System.out.println("修改成功...");
+
+        List<BrandEntity> brandList = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 13L));
+        brandList.forEach(System.out::println);
     }
 
 }
